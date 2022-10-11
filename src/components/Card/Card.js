818e8d197694a0d423119a29ed5537be039c9f15
KubaGithub92 <jakub.rosol@email.cz>
2022-10-11 21:14:38 +0200

    added weather icons, fixed loading data without coordinates

---
+++ src/components/Card/Card.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react'
+import './Card.css'
+import { CloudSun, CloudLightning, Sun, Snowflake, Lightning,CloudRain ,CloudFog, CloudMoon,Cloud,Moon,Wind,ThermometerCold,ThermometerHot,ThermometerSimple } from "phosphor-react";
+
+
+// creating card component
+export default function Card() {
+  // setting states
+  const [weatherData, setWeatherData] = useState(null)
+  const [weatherLoaded, setWeatherLoaded] = useState(false)
+  const [lat, setLat] = useState(null)
+  const [lon, setLon] = useState(null)
+  const absoluteZero = 273.15;
+
+
+  useEffect(()=>{
+    // Getting location of the user
+    navigator.geolocation.getCurrentPosition(function(position) {
+      // setting latitude and longitude
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+       });
+  
+    // Fetching weather data
+   const getWeather = async()=>{
+      const res = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${+lat}&lon=${+lon}&appid=2fbfbc253342eb2b91b2823c547a3b12`)
+      const data =  await res.json();
+      setWeatherData(data);
+      setWeatherLoaded(true)
+    }
+
+
+  if(lat && lon){
+    getWeather();
+    console.log(weatherData);
+    }
+  },[lat,lon])
+
+  // getting todays date
+  const today = () =>{
+    const date = new Date;
+    return date.getDay();
+  }
+
+  // Converting number into actual day
+  let weekDay;
+
+  switch(today()){
+    case 0:
+      weekDay = "Sunday";
+      break;
+    case 1:
+      weekDay = 'Monday';
+      break;
+    case 2:
+      weekDay = "Tuesday";
+      break;
+    case 3:
+      weekDay = 'Wednesday';
+      break;
+    case 4:
+      weekDay = "Thursday";
+      break;
+    case 5:
+      weekDay = 'Friday';
+      break;
+    case 6:
+      weekDay = "Saturday";
+      break;
+  }
+  console.log(weekDay);
+
+  return (
+    <>
+    {/* if weather loaded render jsx */}
+    { weatherLoaded ?
+    <div className='card'>
+      <div className="card__top">
+        <div className='card__top-day'>{weekDay}</div>
+        <div className='card__top-city'>{weatherData.name}</div>
+        <Sun size={240} />
+        {/* <div className="card__top-weather">{weatherData.weather[0].description}</div> */}
+        <div className='card__top-temp'>
+          <ThermometerSimple size={24}/>
+          <div className='card__top-temp-deg'>{Math.round(weatherData.main.temp-absoluteZero)}°C</div>
+        </div>
+        <div className='card__top-degrees'>
+          <div className='degrees__max'><span>Max </span>{Math.round(weatherData.main.temp_max-absoluteZero)}°C</div>
+          <div className='degrees__min'><span>Min </span>{Math.round(weatherData.main.temp_min-absoluteZero)}°C</div>
+        </div>
+      </div>
+
+      <div className="card__border"></div>
+
+      <div className="card__mid"></div>
+      <div className="card__bottom"></div>
+      
+    </div>
+    : <p>DATA LOADING</p>
+}
+</>
+  )
+
+
+}
